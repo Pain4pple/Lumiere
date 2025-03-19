6f46d106fd10460b4a202baddf30dc114afbd12d
Lara Nicole Gatchalian <105093357+Pain4pple@users.noreply.github.com>
2025-03-19 08:44:31 +0800

    init(MovieApp): initial commit with mdd folder structure

---
+++ movie_app/lib/features/auth/presentation/screens/signup_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+
+  _buildTextField() {
+    return Container(
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: const Color.fromARGB(255, 202, 201, 199)),
+      child: TextFormField(decoration: InputDecoration(border: InputBorder.none)),
+    );
+  }
+
+  _buildButton(String text) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: const Color.fromARGB(255, 202, 201, 199)),
+      child: TextButton(onPressed: () {}, child: Text(text)),
+    );
+  }
+}
